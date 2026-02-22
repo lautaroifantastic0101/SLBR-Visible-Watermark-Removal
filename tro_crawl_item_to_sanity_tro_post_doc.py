9a6681a463333ee605898bd9ae7c64906ddfe6ac
lautaroifantastic0101 <lautaroifantastic0101@gmail.com>
2026-02-22 17:05:16 +0800

    update

--- tro_crawl_item_to_sanity_tro_post_doc.py
+++ tro_crawl_item_to_sanity_tro_post_doc.py
@@ -419,7 +419,7 @@ def select_crawl_item_rows_by_case_number(client, account_id, database_id, case_
 def create_sanity_doc_by_case_number(client, account_id, database_id, case_number: str):
     "根据case number查询对应的rows，并且返回一个组装好的sanity doc"
     rows = select_crawl_item_rows_by_case_number(client, account_id, database_id, case_number)
-    print(rows)
+    print(json.dumps(rows))
     if len(rows) > 0:
         ret = row_to_tro_post_doc(rows[0])
         print(json.dumps(ret))
