1d2cccb85ea7f0934fd6840e958020ad8fdaf0f4
lautaroifantastic0101 <lautaroifantastic0101@gmail.com>
2026-02-22 14:50:35 +0800

    update

--- tro_crawl_item_process.py
+++ tro_crawl_item_process.py
@@ -141,14 +141,19 @@ def update_is_multi_case_number(client, account_id, database_id):
         b = ','.join(content_case_numbers)
         c = ','.join(case_number_list)
 
-        print(extract_us_state(content))
-
+        
+        ##########################################
+        # 提取court信息
+        ##########################################
+        court_info = extract_us_state(content) 
+        if court_info is None:
+            court_info = ''
         
     
         ##########################################
         # 生成更新的sql and 执行
         ##########################################
-        update_sql = f'UPDATE tro_crawl_item_tb SET is_multi_case_number = {is_multi}, extract_case_number = "{extract_case_num_column}", case_number_arr = "{case_number_arr_json}", title_case_arr="{a}",  content_case_arr="{b}", origin_case_arr="{c}" WHERE id = {rid}'
+        update_sql = f'UPDATE tro_crawl_item_tb SET is_multi_case_number = {is_multi}, extract_case_number = "{extract_case_num_column}", case_number_arr = "{case_number_arr_json}", title_case_arr="{a}",  content_case_arr="{b}", origin_case_arr="{c}", extract_court="{court_info}"  WHERE id = {rid}'
         update_sql_arr.append(update_sql)
 
 
