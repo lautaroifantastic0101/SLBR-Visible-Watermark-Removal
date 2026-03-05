da478f42d27dfd0a5283f7e991025ce7bc4f568c
lautaroifantastic0101 <lautaroifantastic0101@gmail.com>
2026-03-05 18:53:21 +0800

    update

--- tro_crawl_item_process.py
+++ tro_crawl_item_process.py
@@ -294,8 +294,8 @@ def main():
     if update_case_brief:
         print("只是更新case_brief字段")
         update_sqls = complete_case_brief_column(account_id, cf_ai_api_token, rows)
-        print(update_sqls)
-        return 
+        # print(update_sqls)
+        # return 
         
         
     else:
